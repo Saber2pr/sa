ecf8dbe3fdd76a1a6b8faeedf15da80fa5b7b23f
saber2pr <saber2pr@gmail.com>
2021-08-22 14:47:20 +0800

    feat: search

---
+++ scripts/s.sh
@@ -0,0 +1,37 @@
+platform=$1
+keywords=$2
+
+if [ "$platform" = "" ]; then
+  echo "The platform is required"
+  exit
+fi
+
+if [ "$keywords" = "" ]; then
+  echo "The keywords is required"
+  exit
+fi
+
+url=""
+
+if [[ "$platform" = "blog" || "$platform" = "b" ]]; then
+  url="http://saber2pr.top/#?q=$keywords"
+elif [[ "$platform" = "google" || "$platform" = "g" ]]; then
+  url="https://www.google.com/search?q=$keywords"
+elif [[ "$platform" = "github" || "$platform" = "gh" ]]; then
+  url="https://github.com/search?q=$keywords"
+elif [[ "$platform" = "baidu" || "$platform" = "bd" ]]; then
+  url="https://www.baidu.com/s?wd=$keywords"
+else
+  echo "unknown platform: $platform"
+  exit
+fi
+
+function search_url {
+  if [ "$OSTYPE" == 'msys' ]; then
+    start $url
+  else 
+    open $url
+  fi
+}
+
+search_url
