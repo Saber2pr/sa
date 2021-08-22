e3bc7403c776525d54e470e928b34c12cde617ab
saber2pr <saber2pr@gmail.com>
2021-08-22 20:27:57 +0800

    feat: ipv4

---
+++ scripts/ipv4.sh
@@ -0,0 +1,17 @@
+echo "$(node -p '
+const os = require("os") 
+
+function getLocalIP() {
+  const interfaces = os.networkInterfaces()
+  for (const name of Object.keys(interfaces)) {
+    for (const interf of interfaces[name]) {
+      const { address, family, internal } = interf
+      if (family === "IPv4" && !internal) {
+        return address
+      }
+    }
+  }
+}
+
+getLocalIP()'
+)"
