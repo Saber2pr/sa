d4092a2398137af8d29350655b0d2349c1c085d8
saber2pr <saber2pr@gmail.com>
2021-08-29 18:10:30 +0800

    feat: nest

--- scripts/nest-m.sh
+++ scripts/nest-m.sh
@@ -1,4 +1,23 @@
+gen_entity() {
+  name=$1
+  echo "\
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class ${name^}Entity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('varchar', { length: 255, default: '' })
+  name: string;
+
+  @Column('varchar', { length: 255, default: '' })
+  value: string;
+}" > ./src/modules/$1/$1.entity.ts
+}
+
 nest g s $1 modules \
 && nest g co $1 modules \
 && nest g mo $1 modules \
-&& rm src/modules/$1/*.spec.ts
+&& rm src/modules/$1/*.spec.ts \
+&& gen_entity $1
