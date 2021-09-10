8eab8b16c724b2bdb480bc4decf643b203249a3e
muhai <muhai.li@tuya.com>
2021-09-10 12:59:54 +0800

    chore: update

--- scripts/git-filter-hans.sh
+++ scripts/git-filter-hans.sh
@@ -1,1 +1,3 @@
-FILTER_BRANCH_SQUELCH_WARNING=1 git filter-branch -f --msg-filter "perl -CIOED -p -e 's/\p{Script_Extensions=Han}/ /g'" -- --all
+
+branch=${1:-master}
+FILTER_BRANCH_SQUELCH_WARNING=1 git filter-branch -f --msg-filter "perl -CIOED -p -e 's/\p{Script_Extensions=Han}/ /g'" $branch
