cbde8b6d014d4b81f98e9bc020f419596a909b59
muhai <muhai.li@tuya.com>
2021-09-17 17:51:40 +0800

    chore: update

---
+++ scripts/git-report-day.sh
@@ -0,0 +1,1 @@
+echo -e "$(date +'%Y-%m-%d')日报 $(git config user.name)\n$(git log master --reverse --pretty=format:"%cd %s" --since="date '+%Y-%m-%d 00:00:00'" --date=format:'%H:%M:%S' --grep="[a-z]" --invert-grep --extended-regexp --no-merges)" > $(git config user.name)的$(date +'%Y-%m-%d')日报.txt
