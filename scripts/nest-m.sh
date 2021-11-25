94fa448736f307b12ac061a252f8f9824e791ccc
muhai <muhai.li@tuya.com>
2021-11-25 15:24:08 +0800

    chore: update

--- scripts/nest-m.sh
+++ scripts/nest-m.sh
@@ -1,4 +1,7 @@
-modulePath="./src/modules"
+moduleName=$1
+
+moduleDir=${2:-"./src/modules"}
+modulePath=$moduleDir/$moduleName
 
 headUpperCase() {
   name=$1
@@ -8,27 +11,27 @@ headUpperCase() {
 
 gen_controller() {
   name=$1
-  Name=$(headUpperCase name)
+  Name=$(headUpperCase $name)
   echo "import { Controller } from '@nestjs/common';
 
 @Controller('$name')
 export class ${Name}Controller {}
-" > $modulePath/$1/$1.controller.ts
+" > $modulePath/$1.controller.ts
 }
 
 gen_service() {
   name=$1
-  Name=$(headUpperCase name)
+  Name=$(headUpperCase $name)
   echo "import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class ${Name}Service {}
-" > $modulePath/$1/$1.service.ts
+" > $modulePath/$1.service.ts
 }
 
 gen_module() {
   name=$1
-  Name=$(headUpperCase name)
+  Name=$(headUpperCase $name)
   echo "import { ${Name}Service } from './${name}.service';
 import { ${Name}Controller } from './${name}.controller';
 import { Module } from '@nestjs/common';
@@ -39,9 +42,10 @@ import { Module } from '@nestjs/common';
   exports: [${Name}Service],
 })
 export class ${Name}Module {}
-" > $modulePath/$1/$1.module.ts
+" > $modulePath/$1.module.ts
 }
 
+mkdir -p $moduleDir
 mkdir $modulePath
 
 gen_controller $1
