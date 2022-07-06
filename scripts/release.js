8dd0a1d17323d177463380567c3d073bd7ad7bd3
muhai <muhai.li@tuya.com>
2022-07-06 17:11:26 +0800

    chore: update

--- scripts/release.js
+++ scripts/release.js
@@ -1,11 +1,11 @@
-const child_process = require('child_process')
-const { join } = require('path')
+const child_process = require('child_process');
+const { join } = require('path');
 
-const pkg = require(join(process.cwd(), 'package.json'))
+const pkg = require(join(process.cwd(), 'package.json'));
 
-const version = pkg.version
-const length = version.length
+const version = pkg.version;
+const index = version.lastIndexOf('.') + 1;
 
-const newVersion = `${version.slice(0, length - 1)}${Number(version[length - 1]) + 1}`
+const newVersion = `${version.slice(0, index)}${Number(version.slice(index)) + 1}`;
 
 child_process.exec(`yarn version --new-version ${newVersion}`)
