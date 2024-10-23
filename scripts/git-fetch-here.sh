96a74c69fb795a616bb6db3ae902ae6940d90e5f
muhai <muhai.li@tuya.com>
2024-10-23 10:30:07 +0800

    chore: update

--- scripts/git-fetch-here.sh
+++ scripts/git-fetch-here.sh
@@ -1,19 +1,11 @@
 name=.temp
 repo=$1
 
-# save old origin
-originUrl=$(git remote get-url origin)
-
 # clone new repo
 git clone --depth 1 $repo $name
+
+rm -rf $name/.git
 
 # copy new repo files
 ls -A1 $name | xargs -I {} cp -r $name/{} ./
 rm -rf ./.temp
-
-# clear current git
-rm -rf .git
-git init
-
-# set old origin
-git remote add origin $originUrl
