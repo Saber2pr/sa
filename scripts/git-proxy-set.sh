8e2fd76465f851ce820a00f3f3a5610f2c1cb394
saber2pr <muhai.li@tuya.com>
2026-01-18 15:04:41 +0800

    chore: update

--- scripts/git-proxy-set.sh
+++ scripts/git-proxy-set.sh
@@ -1,2 +1,2 @@
-git config --global https.proxy http://localhost:10809/
-git config --global http.proxy http://localhost:10809/
+git config --global https.proxy http://localhost:7890/
+git config --global http.proxy http://localhost:7890/
